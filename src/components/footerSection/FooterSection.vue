f0c21d61378decd91fc7a369891bc84739b2bd37
dahakalab <dahakalab@gmail.com>
2018-12-04 01:14:13 +0300

    Create layout components header and footer

---
+++ src/components/footerSection/FooterSection.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  FooterSection
+    footer
+      h3 footer
+
+</template>
+
+<script>
+  export default {
+    // name: 'FooterSection',
+    data () {
+      return {}
+    }
+  }
+</script>
---
+++ src/components/headerSection/HeaderSection.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  HeaderSection
+    header
+      h1 header
+
+</template>
+
+<script>
+  export default {
+    // name: 'HeaderSection',
+    data () {
+      return {}
+    }
+  }
+</script>
